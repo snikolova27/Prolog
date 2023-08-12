bc1449baa1f2ac67e8d21a15294439fa3d0642f9
Sonya Nikolova <snikolova27@gmail.com>
2023-08-12 12:05:21 +0300

    add more generators

--- Lists/lists.pl
+++ Lists/lists.pl
@@ -60,3 +60,48 @@ range_sonik([], LowerBound, UpperBound) :- UpperBound < LowerBound.
 range_sonik( [LowerBound | T], LowerBound, UpperBound) :- LowerBound =< UpperBound, 
                     NewLowerBound is LowerBound + 1,
                     range_sonik(T, NewLowerBound, UpperBound).
+                
+% nat(N) -> checks if N is a natural number
+nat(0).
+nat(N) :- nat(K), N is K + 1.
+
+% gen_KS(Len, Sum, List) -> generates a list with length Len and Sum of its members equal to Sum
+gen_KS(1,Sum, [Sum]).
+gen_KS(Len, Sum, [Elem | R]) :-
+    Len > 1,
+    between_sonik(Elem, 0, Sum),
+    NewSum is Sum - Elem,
+    NewLen is Len - 1,
+    gen_KS(NewLen, NewSum, R).
+
+% gen_nat_pair(A,B) -> generates a pair of natural numbers
+gen_nat_pair(A, B):-
+    nat(Sum),
+    gen_KS(2, Sum, [ A | B ])
+    
+% int(N,Z) -> generate Z belonging to the whole numbers set
+int(0, 0).
+int(N, N) :- N > 0.
+int(N, Z) :- N > 0, Z is -N.
+
+% gen_arithmethic_progression(List)
+gen_arithmethic_progression([]).
+gen_arithmethic_progression(Progression) :-
+    nat(N),
+    gen_KS(3, N, [L, K, M]),
+    L > 0, % length 
+    int(K, A0),
+    int(M, D), % step / difference between elements
+    gen_arithmethic_progression(L, A0, D, Progression).
+
+gen_arithmethic_progression(1, A0, 0, [A0]).
+gen_arithmethic_progression(L, A0, D, Progression) :-
+    L > 1,
+    gen_ar_prog_with_rec(L, A0, D, Progression).
+
+gen_ar_prog_with_rec(1, A0, _, [A0]).
+gen_ar_prog_with_rec(L, A0, D, [A0 | Progression]) :-
+    L > 1,
+    L1 is L - 1,
+    A1 is A0 + D,
+    gen_ar_prog_with_rec(L1, A1, D, Progression).
